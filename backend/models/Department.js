5708200cbe50765b7c5b571fb23ad0d7ed617750
mrtuyen87 <mrtuyen.87@gmail.com>
2026-03-27 12:02:50 +0700

    Create Department schema model file

---
+++ backend/models/Department.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const departmentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Department', departmentSchema);
